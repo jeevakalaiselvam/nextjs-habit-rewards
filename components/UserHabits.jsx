aa886c48ede240bc61369c034e09a5e5afd1656b
jeevakalaiselvam <jeevak001@outlook.com>
2025-01-20 18:28:05 +0530

    Fix

--- components/UserHabits.jsx
+++ components/UserHabits.jsx
@@ -193,6 +193,17 @@ export default function UserHabits({ setActiveItem, currentDate, user }) {
     }
   });
 
+  useEffect(() => {
+    let categoryForUsers = CATEGORY_OPTIONS?.filter((category) => {
+      const { id, value } = category;
+      const habitsForCategory = habits?.filter((habit) => {
+        return habit?.category == id;
+      });
+      return habitsForCategory?.length > 0;
+    });
+    setSelectedFitler(categoryForUsers?.[0]?.id);
+  }, [user, habits]);
+
   return (
     <Container>
       {loading && (
