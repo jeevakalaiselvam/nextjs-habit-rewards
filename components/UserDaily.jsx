ed77a98ae2c7fd872137ce61c6569caf2d094c68
jeevakalaiselvam <jeevak001@outlook.com>
2025-01-21 15:58:06 +0530

    User effect added

--- components/UserDaily.jsx
+++ components/UserDaily.jsx
@@ -68,7 +68,7 @@ export default function UserHabitLog({ currentDate, user }) {
   useEffect(() => {
     refreshHabits();
     refreshHabitLogs();
-  }, [currentDate]);
+  }, [currentDate, user]);
 
   const todayIdentifier = moment(new Date(currentDate)).format('YYYY-MM-DD');
 
