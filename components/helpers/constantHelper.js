e804a29392eb1f8f9ebac9a6fe2f7fdc2efade04
jeevakalaiselvam <jeevak001@outlook.com>
2025-01-28 14:28:17 +0530

    Changes

--- components/helpers/constantHelper.js
+++ components/helpers/constantHelper.js
@@ -11,6 +11,7 @@ export const CATEGORY_OPTIONS = [
   { id: 'House', value: 'House' },
   { id: 'Food', value: 'Food' },
   { id: 'Base', value: 'Base' },
+  { id: 'Others', value: 'Others' },
 ];
 
 export const MULTI_OPTIONS = [
