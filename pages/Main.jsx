858d9ec0de6dc96d5668c98d1dd761fa844151ca
jeevakalaiselvam <jeevak001@outlook.com>
2025-05-16 03:07:41 +0530

    Fix

--- pages/Main.jsx
+++ pages/Main.jsx
@@ -265,7 +265,6 @@ export default function Main() {
         <HRight>
           <AddIcon
             onClick={() => {
-              setShowRecentAchUnlock(true);
               refreshAchievements();
             }}
           >
