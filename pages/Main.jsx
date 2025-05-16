439e577e5137ba6de04b78a94103575e9bbb1885
jeevakalaiselvam <jeevak001@outlook.com>
2025-05-16 17:01:58 +0530

    Changes

--- pages/Main.jsx
+++ pages/Main.jsx
@@ -294,7 +294,7 @@ export default function Main() {
                 marginRight: '.25rem',
               }}
             >
-              NeverTheEzWay - {achievements?.length}
+              NotRealLogan - {achievements?.length}
             </span>
             <span style={{ fontSize: '1.1rem', color: COLOR_GREEN }}>
               <FaTrophy />
