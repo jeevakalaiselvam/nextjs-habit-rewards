2132c5952f6798d7f4197d267eaea75635fbfd28
jeevakalaiselvam <jeevak001@outlook.com>
2025-05-18 19:25:13 +0530

    Changes

--- pages/Main.jsx
+++ pages/Main.jsx
@@ -1313,6 +1313,10 @@ const Section = styled.div`
   border-radius: 4px 4px 0px 0px;
   color: ${(props) => (props.selected ? COLOR_BLUE_LIGHT : COLOR_GREY)};
   cursor: pointer;
+
+  &:hover {
+    color: ${(props) => COLOR_BLUE_LIGHT};
+  }
 `;
 
 const Middle = styled.div`
