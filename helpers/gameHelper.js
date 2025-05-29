e9231be48221c876e15bb551934499b3ed63870f
Jeeva Kalaiselvam <jeevak001@outlook.com>
2025-05-29 15:06:37 +0530

    Changes

--- helpers/gameHelper.js
+++ helpers/gameHelper.js
@@ -1,15 +1,16 @@
-export const Battlefield2042 = 'Battlefield 2042';
-export const Hitman = 'Hitman World of Assassination';
+
 
 export const ICON_MAPPER = {
-  [Battlefield2042]:
+  "Battlefield 2042":
     'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/apps/1517290/59d040bb9b67112e982261a71d93ed033740ac4c.jpg',
-  [Hitman]:
+  "Hitman World of Assassination":
     'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/apps/1659040/79c615cd7d3feef4deb072173a36ee546e916615.jpg',
+  "Dex":
+    'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/apps/269650/37dd7d1b585764249e35566fc1afca5c76de340b.jpg',
 };
 
 
-export const GAMES_ARRAY = ['Battlefield 2042', 'Hitman World of Assassination'];
+export const GAMES_ARRAY = ['Battlefield 2042', 'Hitman World of Assassination',"Dex"];
 
 
 
