ccafb3d9d64af74fa65d66801ce73fc8b0b0aa79
Jeeva Kalaiselvam <jeevak001@outlook.com>
2025-06-22 11:04:39 +0530

    Changes

--- pages/Main.jsx
+++ pages/Main.jsx
@@ -985,7 +985,7 @@ export default function Main() {
                         <A1Progress1>
                           <Progress
                             strokeColor={COLOR_ACCENT}
-                            size={[10, 7]}
+                            size={[20, 7]}
                             steps={MAX_FOR_COUNT}
                             percent={(
                               (ach?.completed / ach?.total) *
