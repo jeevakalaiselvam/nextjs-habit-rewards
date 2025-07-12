37e5f5a3ea333170fee2e53ab303fca3c7dbf3f8
Jeeva Kalaiselvam <jeevak001@outlook.com>
2025-07-12 08:37:42 +0530

    Fix

--- components/MainContent.js
+++ components/MainContent.js
@@ -750,7 +750,22 @@ export default function MainContent({
                           achieved={ach?.achieved}
                         >
                           <AchIconOuter achieved={ach?.achieved}>
-                            <AchIcon icon={ach?.icon}></AchIcon>
+                            <AchIcon
+                              icon={ach?.icon}
+                              onClick={() => {
+                                if (window !== "undefined") {
+                                  const searchQuery = `${
+                                    ach?.displayName
+                                  } achievement ${encodeURIComponent(
+                                    ach?.gameName
+                                  )} `;
+                                  window.open(
+                                    `https://www.google.com/search?q=${searchQuery}`
+                                  );
+                                  // window.open(`https://www.youtube.com/results?search_query=${searchQuery}`);
+                                }
+                              }}
+                            ></AchIcon>
                           </AchIconOuter>
                           <AchData>
                             <AchTitle>{ach?.displayName}</AchTitle>
@@ -916,7 +931,22 @@ export default function MainContent({
                               achieved={ach?.achieved}
                             >
                               <AchIconOuter achieved={ach?.achieved}>
-                                <AchIcon icon={ach?.icon}></AchIcon>
+                                <AchIcon
+                                  icon={ach?.icon}
+                                  onClick={() => {
+                                    if (window !== "undefined") {
+                                      const searchQuery = `${
+                                        ach?.displayName
+                                      } achievement ${encodeURIComponent(
+                                        ach?.gameName
+                                      )} `;
+                                      window.open(
+                                        `https://www.google.com/search?q=${searchQuery}`
+                                      );
+                                      // window.open(`https://www.youtube.com/results?search_query=${searchQuery}`);
+                                    }
+                                  }}
+                                ></AchIcon>
                               </AchIconOuter>
                               <AchData>
                                 <AchTitle>{ach?.displayName}</AchTitle>
