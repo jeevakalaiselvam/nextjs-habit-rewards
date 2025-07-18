6710ee839a55253453cc93386dffd8f01eca6ec3
Jeeva Kalaiselvam <jeevak001@outlook.com>
2025-07-18 19:11:19 +0530

    Fix

--- pages/Main.jsx
+++ pages/Main.jsx
@@ -113,7 +113,7 @@ export default function Atom() {
         achievements: [
           ...sortedPlatinumTrophies,
           {
-            displayName: `${game?.name} Platinum`,
+            displayName: `Platinum`,
             description: `Achieved all Trophies in game`,
             hiddenDesc: `${game?.name}`,
             percentage: lastAch?.percentage,
