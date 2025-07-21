a2bd9c6d14ce50ca2dc73be9f102c8b1945f2fd9
Jeeva Kalaiselvam <jeevak001@outlook.com>
2025-07-21 15:18:19 +0530

    Fix

--- components/MainHeader.js
+++ components/MainHeader.js
@@ -137,6 +137,21 @@ export default function MainHeader({
         </HeaderProfileLevel>
       </HeaderName>
       <HeaderCounts>
+        <Section color={COLOR_WHITE}>
+          <Top>
+            <span
+              style={{
+                transform: "translateY(-2.5px)",
+                marginRight: ".25rem",
+              }}
+            >
+              <PlatinumIcon />
+            </span>
+            <span style={{ fontSize: "1.5rem", fontWeight: " bolder" }}>
+              {total}
+            </span>
+          </Top>
+        </Section>
         <Section color={COLOR_PLATINUM}>
           <Top>
             <span
