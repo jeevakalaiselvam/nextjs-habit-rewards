2277adfb0f36f5c827358dcb23861694d896f3ab
Jeeva Kalaiselvam <jeevak001@outlook.com>
2025-08-11 00:36:42 +0530

    Change

--- components/MainContent.js
+++ components/MainContent.js
@@ -674,10 +674,6 @@ export default function MainContent({
                         ...(selectedGame?.achievements ?? [])?.filter(
                           (ach) => ach?.color == "Platinum"
                         ),
-                        ...(selectedGame?.achievements ?? [])?.filter(
-                          (ach) =>
-                            ach?.color != "Platinum" && ach?.achieved != 1
-                        ),
                         ...(selectedGame?.achievements ?? [])
                           ?.filter(
                             (ach) =>
@@ -686,6 +682,10 @@ export default function MainContent({
                           ?.sort(
                             (ach1, ach2) => ach2?.unlocktime - ach1?.unlocktime
                           ),
+                        ...(selectedGame?.achievements ?? [])?.filter(
+                          (ach) =>
+                            ach?.color != "Platinum" && ach?.achieved != 1
+                        ),
                       ]?.map((ach, index) => {
                         let desc1 = ach?.hiddenDesc;
                         let desc2 = ach?.description;
