563144316fa3502f6daea9c2e4f14123fd9aecaa
Jeeva Kalaiselvam <jeevak001@outlook.com>
2025-08-11 19:01:50 +0530

    Changes

--- components/MainContent.js
+++ components/MainContent.js
@@ -552,9 +552,16 @@ export default function MainContent({
                             >
                               {game?.name}
                             </GameTitle>
-                            <GameCompletion>
-                              {completed} of {total} Trophies
-                            </GameCompletion>
+                            {completed < total && (
+                              <GameCompletion>
+                                {completed} of {total} Trophies
+                              </GameCompletion>
+                            )}
+                            {completed == total && (
+                              <GameCompletion>
+                                All {total} Trophies
+                              </GameCompletion>
+                            )}
                             <GameLastPlayed>
                               {lastUnlocked &&
                                 formatDate(
