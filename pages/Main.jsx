6386fc72b825a757882c1297bd63bbc017548ea8
Jeeva Kalaiselvam <jeevak001@outlook.com>
2025-08-11 16:06:01 +0530

    Changes

--- pages/Main.jsx
+++ pages/Main.jsx
@@ -108,7 +108,7 @@ export default function Atom() {
           ...sortedPlatinumTrophies,
           {
             displayName: `Platinum`,
-            description: `Achieved all base game trophies in the game`,
+            description: `Achieved all Base Game Trophies in the game`,
             hiddenDesc: `${game?.name}`,
             percentage: lastAch?.percentage,
             label: getRarityBasedOnRarity(lastAch?.percentage),
