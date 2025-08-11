73b198495091e3910900c276dc2f4e324c36b2d3
Jeeva Kalaiselvam <jeevak001@outlook.com>
2025-08-11 16:51:29 +0530

    Changes

--- components/MainContent.js
+++ components/MainContent.js
@@ -326,20 +326,6 @@ export default function MainContent({
           >
             GAMES
           </TabLink>
-          <TabLink
-            onClick={() => {
-              setSelected("LIBRARY");
-              setSelectedMode("LIBRARY");
-              if (window) {
-                localStorage.setItem("SELECTED_TAB", "LIBRARY");
-              }
-            }}
-            active={selectedMode == "LIBRARY"}
-            onMouseEnter={() => setActive("LIBRARY")}
-            onMouseLeave={() => setActive("")}
-          >
-            LIBRARY
-          </TabLink>
         </FRRight>
         <FRRight>
           <TabLink
