6db0437042e7dab51a475376af942a8e71df02ed
Jeeva Kalaiselvam <jeevak001@outlook.com>
2025-08-13 19:51:48 +0530

    Chanmge

--- pages/Main.jsx
+++ pages/Main.jsx
@@ -92,6 +92,7 @@ export default function Atom() {
             };
           })
           ?.filter((ach) => {
+            return true;
             if (platinumMapper[ach?.displayName]) {
               return true;
             } else {
