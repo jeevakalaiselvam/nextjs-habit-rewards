78938ebeaaa53c1aeeb464b0fda62ebbfb33f810
Jeeva Kalaiselvam <jeevak001@outlook.com>
2025-08-13 13:44:25 +0530

    Changes

--- pages/Main.jsx
+++ pages/Main.jsx
@@ -59,73 +59,79 @@ export default function Atom() {
   useEffect(() => {
     let finalGames = [];
 
-    finalGames = games?.map((game) => {
-      let platinumGameData = platinumData?.find((item) => item?.id == game?.id);
-      let formedGame = {};
-      let platinumMapper = {};
-      let dlcMapper = {};
+    const GAMES_INCLUDED = ["1659040", "2358720"];
 
-      platinumGameData?.platinum?.forEach((ach) => {
-        platinumMapper[ach?.title] = ach;
+    finalGames = games
+      ?.filter((game) => GAMES_INCLUDED?.includes(String(game?.id)))
+      ?.map((game) => {
+        let platinumGameData = platinumData?.find(
+          (item) => item?.id == game?.id
+        );
+        let formedGame = {};
+        let platinumMapper = {};
+        let dlcMapper = {};
+
+        platinumGameData?.platinum?.forEach((ach) => {
+          platinumMapper[ach?.title] = ach;
+        });
+
+        console.log({ platinumMapper });
+
+        let gameName = game?.achievements?.[0]?.gameName;
+
+        let sortedPlatinumTrophies = game?.achievements
+          ?.map((ach) => {
+            return {
+              ...ach,
+              label: getRarityBasedOnRarity(ach?.percentage),
+              color: getColorBasedOnRarity(ach?.percentage),
+              title: ach?.displayName,
+              hiddenDesc: platinumMapper[ach?.displayName]?.description,
+            };
+          })
+          ?.filter((ach) => {
+            if (platinumMapper[ach?.displayName]) {
+              return true;
+            } else {
+              return false;
+            }
+          })
+          ?.sort((ach1, ach2) => +ach2.percentage - +ach1?.percentage);
+
+        let lastAch =
+          sortedPlatinumTrophies?.[sortedPlatinumTrophies?.length - 1];
+
+        let total = sortedPlatinumTrophies?.length;
+        let completed = sortedPlatinumTrophies?.filter(
+          (ach) => ach?.achieved == "1"
+        )?.length;
+        let isCompleted = completed > Math.ceil(total * COMPLETION_FACTOR);
+
+        formedGame = {
+          ...game,
+          ...platinumGameData,
+          achievements: [
+            ...sortedPlatinumTrophies?.filter(
+              (ach) => ach?.displayName != lastAch?.displayName
+            ),
+            { ...lastAch, color: "Gold" },
+            {
+              displayName: `Platinum`,
+              description: `Achieved all Base Game Trophies in the game`,
+              hiddenDesc: `${game?.name}`,
+              percentage: lastAch?.percentage,
+              label: getRarityBasedOnRarity(lastAch?.percentage),
+              color: "Platinum",
+              achieved: isCompleted ? 1 : 0,
+              unlocktime: lastAch?.unlocktime,
+              icon: "https://pbs.twimg.com/media/GF8EZJZWQAAwDR7.jpg",
+              gameName: lastAch?.gameName,
+            },
+          ],
+        };
+
+        return formedGame;
       });
-
-      console.log({ platinumMapper });
-
-      let gameName = game?.achievements?.[0]?.gameName;
-
-      let sortedPlatinumTrophies = game?.achievements
-        ?.map((ach) => {
-          return {
-            ...ach,
-            label: getRarityBasedOnRarity(ach?.percentage),
-            color: getColorBasedOnRarity(ach?.percentage),
-            title: ach?.displayName,
-            hiddenDesc: platinumMapper[ach?.displayName]?.description,
-          };
-        })
-        ?.filter((ach) => {
-          if (platinumMapper[ach?.displayName]) {
-            return true;
-          } else {
-            return false;
-          }
-        })
-        ?.sort((ach1, ach2) => +ach2.percentage - +ach1?.percentage);
-
-      let lastAch =
-        sortedPlatinumTrophies?.[sortedPlatinumTrophies?.length - 1];
-
-      let total = sortedPlatinumTrophies?.length;
-      let completed = sortedPlatinumTrophies?.filter(
-        (ach) => ach?.achieved == "1"
-      )?.length;
-      let isCompleted = completed > Math.ceil(total * COMPLETION_FACTOR);
-
-      formedGame = {
-        ...game,
-        ...platinumGameData,
-        achievements: [
-          ...sortedPlatinumTrophies?.filter(
-            (ach) => ach?.displayName != lastAch?.displayName
-          ),
-          { ...lastAch, color: "Gold" },
-          {
-            displayName: `Platinum`,
-            description: `Achieved all Base Game Trophies in the game`,
-            hiddenDesc: `${game?.name}`,
-            percentage: lastAch?.percentage,
-            label: getRarityBasedOnRarity(lastAch?.percentage),
-            color: "Platinum",
-            achieved: isCompleted ? 1 : 0,
-            unlocktime: lastAch?.unlocktime,
-            icon: "https://pbs.twimg.com/media/GF8EZJZWQAAwDR7.jpg",
-            gameName: lastAch?.gameName,
-          },
-        ],
-      };
-
-      return formedGame;
-    });
     setFinalGames(finalGames);
   }, [games, platinumData]);
 
