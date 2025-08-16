497b32974816d2c8da12e010d09fcea0009906b1
Jeeva Kalaiselvam <jeevak001@outlook.com>
2025-08-16 16:07:44 +0530

    Changes

--- pages/Main.jsx
+++ pages/Main.jsx
@@ -87,7 +87,6 @@ export default function Atom() {
           };
         })
         ?.filter((ach) => {
-          return true;
           if (platinumMapper[ach?.displayName]) {
             return true;
           } else {
