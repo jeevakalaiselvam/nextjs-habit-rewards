37260630482b8bc851293bd8f3858f6ec73d7735
Jeeva Kalaiselvam <jeevak001@outlook.com>
2025-08-16 15:32:13 +0530

    Changes

--- components/MainContent.js
+++ components/MainContent.js
@@ -595,7 +595,7 @@ export default function MainContent({
                               </GameTitle>
                               {completed < total && (
                                 <GameCompletion>
-                                  {completed} of {total} Trophies
+                                  {completed} Trophies
                                 </GameCompletion>
                               )}
                               {completed == total && (
@@ -959,11 +959,13 @@ export default function MainContent({
                     <Game2Line>
                       {[
                         ...(selectedGame?.achievements ?? [])?.filter(
-                          (ach) => ach?.color == "Platinum"
+                          (ach) => ach?.color == "Platinum" && false
                         ),
                         ...(selectedGame?.achievements ?? [])?.filter(
                           (ach) =>
-                            ach?.color != "Platinum" && ach?.achieved != 1
+                            ach?.color != "Platinum" &&
+                            ach?.achieved != 1 &&
+                            false
                         ),
                         ...(selectedGame?.achievements ?? [])
                           ?.filter(
