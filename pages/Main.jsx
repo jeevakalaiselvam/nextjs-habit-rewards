43143facdf6f1911f1981ce92772ed8c38ff7ade
Jeeva Kalaiselvam <jeevak001@outlook.com>
2025-08-20 21:23:19 +0530

    Changes

--- pages/Main.jsx
+++ pages/Main.jsx
@@ -67,6 +67,7 @@ export default function Atom() {
 
     finalGames = games
       ?.filter((game) => {
+        return true;
         return GAMES_INCLUDED?.includes(String(game?.id));
       })
       ?.map((game) => {
