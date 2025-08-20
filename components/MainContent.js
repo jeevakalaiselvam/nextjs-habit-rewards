2f24298bd7d63dc8118058607a1837cee4f8e7fc
Jeeva Kalaiselvam <jeevak001@outlook.com>
2025-08-20 16:35:14 +0530

    Change

--- components/MainContent.js
+++ components/MainContent.js
@@ -1911,6 +1911,7 @@ const AchCard = styled.div`
   width: 100%;
   background-color: #080c11;
   margin-bottom: 0.5rem;
+  opacity: ${(props) => (props?.achieved == 1 ? 1 : 0.25)};
 `;
 
 const Game2Line = styled.div`
