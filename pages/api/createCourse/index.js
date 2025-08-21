537d339e9c522e0f8e7bd7854aa4a3b4449b7bcc
Jeeva Kalaiselvam <jeevak001@outlook.com>
2025-08-21 23:51:39 +0530

    Changes

--- pages/api/createCourse/index.js
+++ pages/api/createCourse/index.js
@@ -1,8 +1,23 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method === "POST") {
     const { name, exclude, count } = req.body;
     if (!name || !exclude || !count) {
       res.status(404).json({ status: "Error" });
+    }
+
+    try {
+      const client = await clientPromise;
+      const db = client.db("habittracker");
+      await db.collection("saravana").insertOne({
+        name,
+        exclude,
+        count: String(count),
+      });
+
+      res.status(201).json({ message: "Achievement added successfully" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error });
     }
 
     res.status(200).json({ message: "Create Success" });
