67cd7a783e037142ae5b7ff696a156cd460c0bce
Jeeva Kalaiselvam <jeevak001@outlook.com>
2025-08-27 11:32:09 +0530

    Changes

--- pages/Main2.jsx
+++ pages/Main2.jsx
@@ -863,7 +863,7 @@ export default function Main2() {
         </Top>
         {!loading && (
           <Content>
-            {active == "GAME" && (
+            {false && active == "GAME" && (
               <CanvasRight>
                 {loadingTrophies && (
                   <Spin
@@ -1028,7 +1028,7 @@ export default function Main2() {
                       onStop={() => {
                         let lastRefresh = "";
                         let oldId = "";
-                        if (window) {
+                        if (window && false) {
                           lastRefresh =
                             localStorage.getItem("LAST_REFRESH") ?? "";
                           oldId = localStorage.getItem("LAST_GAME") ?? "";
