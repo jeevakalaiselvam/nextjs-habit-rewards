318b963679af8744b467d1855b1d5f2be7b04fa8
Jeeva Kalaiselvam <jeevak001@outlook.com>
2025-11-30 16:05:04 +0530

    Changes

--- ncomponents/NewProfile.jsx
+++ ncomponents/NewProfile.jsx
@@ -1,14 +1,14 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export default function NewProfile() {
   return (
     <Container>
       <Profile></Profile>
       <Name>
-        <span style={{ color: '#3DB1F0', fontSize: '.9rem', height: '20px' }}>
-          NotRealLogan
+        <span style={{ color: "#3DB1F0", fontSize: ".9rem", height: "20px" }}>
+          N7ShadowX
         </span>
-        <span style={{ fontSize: '.8rem', height: '20px' }}>All Library</span>
+        <span style={{ fontSize: ".8rem", height: "20px" }}>All Library</span>
       </Name>
     </Container>
   );
@@ -20,7 +20,7 @@ const Profile = styled.div`
   justify-content: flex-start;
   width: 50px;
   height: 50px;
-  background: url('https://blog.bioware.com/wp-content/uploads/2015/10/TwitterProfile.png');
+  background: url("https://blog.bioware.com/wp-content/uploads/2015/10/TwitterProfile.png");
   background-size: contain;
   background-repeat: no-repeat;
 `;
