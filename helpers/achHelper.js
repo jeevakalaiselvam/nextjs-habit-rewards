d19e7218ea34e262f430e5dc98ed9322beb6d285
jeevakalaiselvam <jeevak001@outlook.com>
2025-12-29 22:04:43 +0530

    Fix

--- helpers/achHelper.js
+++ helpers/achHelper.js
@@ -1,24 +1,24 @@
 export const getColorBasedOnRarity = (percentage) => {
-  if (percentage >= 0 && percentage <= 10) {
-    return "Gold";
-  } else if (percentage > 10 && percentage <= 25) {
-    return "Silver";
-  } else if (percentage > 25 && percentage <= 100) {
-    return "Bronze";
+  if (percentage >= 0 && percentage <= 25) {
+    return 'Gold';
+  } else if (percentage > 25 && percentage <= 50) {
+    return 'Silver';
+  } else if (percentage > 50 && percentage <= 100) {
+    return 'Bronze';
   }
 };
 
 export const getRarityBasedOnRarity = (percentage) => {
   if (percentage > 0 && percentage <= 5) {
-    return "Ultra Rare";
+    return 'Ultra Rare';
   } else if (percentage > 5 && percentage <= 10) {
-    return "Very Rare";
+    return 'Very Rare';
   } else if (percentage > 10 && percentage <= 25) {
-    return "Rare";
+    return 'Rare';
   } else if (percentage > 25 && percentage <= 50) {
-    return "Uncommon";
+    return 'Uncommon';
   } else if (percentage > 50 && percentage <= 100) {
-    return "Common";
+    return 'Common';
   }
 };
 
