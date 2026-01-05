653cd586efc0462a596d71ded0e8109c4cc9050d
jeevakalaiselvam <jeevak001@outlook.com>
2026-01-05 15:17:02 +0530

    Fix

--- components/MainContent.js
+++ components/MainContent.js
@@ -39,7 +39,7 @@ export default function MainContent({
   const [gameSearch, setGameSearch] = useState('');
 
   let sortedGames = games.sort((a, b) => {
-    return b.completion - a.completion;
+    return a.completion - b.completion;
   });
 
   sortedGames = sortedGames?.filter((game) =>
