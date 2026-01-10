b939bb6c269e0ab3b839396a2c2d0990fe31edc1
jeevakalaiselvam <jeevak001@outlook.com>
2026-01-10 14:10:06 +0530

    Fix

--- pages/api/steam/index.js
+++ pages/api/steam/index.js
@@ -159,7 +159,7 @@ const handler = async (req, res) => {
     } catch (error) {
       console.error(error);
       //Get all Games and Refresh data in File
-      res.status(500).json({ status: 'error', error: error });
+      res.status(500).json({ status: 'error', error: JSON.stringify(error) });
     }
   }
 };
